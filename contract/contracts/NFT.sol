cc59f524e248e27f460ac50b610347422c33253c
Charlie Chen <charlie@jomo.network>
2022-11-05 13:48:56 -0700

    [contract] Add NFT contract

---
+++ contract/contracts/NFT.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
+import "@openzeppelin/contracts/utils/Base64.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract NFT is ERC721URIStorage, Ownable {
+    using Strings for uint256;
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+
+    constructor() ERC721("NFT", "NFT") {}
+
+    // Only owner can call this method and mint the NFT to user.
+    function mint(string memory _tokenURI, address user) public onlyOwner {
+        _tokenIds.increment();
+        uint256 newItemId = _tokenIds.current();
+        _safeMint(user, newItemId);
+        _setTokenURI(newItemId, _tokenURI);
+    }
+}
