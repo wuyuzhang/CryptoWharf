40748f334906ac5c08f2606d4d14eae5a920c8e9
Charlie Chen <charlie@jomo.network>
2022-11-05 06:22:53 -0700

    [frontend] Cookiecutter skeleton for frontend

---
+++ frontent/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className='body'>
+      </div>
+      <div className="footer">CryptoWharf</div>
+    </div>
+  );
+}
+
+export default App;
